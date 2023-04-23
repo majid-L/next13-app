51f2c91fc064b32b23085274a3d9fe68dfef9c0a
3782291211 <Laklouk98@hotmail.com>
2023-04-23 12:54:06 +0100

    Adding fields to state object

--- app/context/store.jsx
+++ app/context/store.jsx
@@ -5,10 +5,19 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext(null);
 
 const LoggedInUserContext = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useState(window.localStorage.getItem('ACTIVE_USER'));
-    const [userDetails, setUserDetails] = useState('');
+    const [loggedInUser, setLoggedInUser] = useState(
+        { 
+            user : 
+                { 
+                    name : window.localStorage.getItem('ACTIVE_USER'),
+                    id: window.localStorage.getItem('USER_ID'),
+                    email: window.localStorage.getItem('USER_EMAIL')
+                }, 
+            token: window.localStorage.getItem('AUTH_TOKEN')
+        }
+    );
 
-    return (<GlobalContext.Provider value={{loggedInUser, setLoggedInUser, userDetails, setUserDetails}}>
+    return (<GlobalContext.Provider value={{ loggedInUser, setLoggedInUser }}>
         {children}
     </GlobalContext.Provider>)
 }
