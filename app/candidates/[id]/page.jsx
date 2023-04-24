ccb1e1f4131f89c18ac5e42bb308d2cb6b7e7eea
3782291211 <Laklouk98@hotmail.com>
2023-04-24 18:00:15 +0100

    Adding page for single candidate's exams

---
+++ app/candidates/[id]/page.jsx
@@ -0,0 +1,17 @@
+'use client';
+import Exams from '../../components/Exams';
+import useFetchExams from '../../hooks/useFetchExams';
+
+const SingleCandidateExams = ({params}) => {
+const {exams, isLoading} = useFetchExams(30, params.id);
+
+return (
+<main className="text-center mt-20 pb-20 mx-auto w-11/12 sm:w-5/6">
+  <h1 className="text-center text-stone-100 font-bold text-4xl md:text-5xl mt-20">Viewing exam sessions for {exams[0]?.candidateName}</h1>
+  <p className="text-center text-stone-100 mt-2 mb-16 md:text-xl">To see all exams for a specific student, click on an exam and you will be taken to the relevant page.</p>
+  <Exams exams={exams} isLoading={isLoading}/>
+</main>
+);
+};
+
+export default SingleCandidateExams;
