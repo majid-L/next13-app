c0cc53c17ede3ee6db557bde27c60650cb6d092d
3782291211 <Laklouk98@hotmail.com>
2023-04-24 06:53:16 +0100

    Adding conditional rendering

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -3,30 +3,25 @@ import { useState, useContext, useEffect, useMemo, useLayoutEffect } from "react
 import { GlobalContext } from "../context/store";
 import { getExams } from '../api/apiRequests';
 import React from "react";
-import BingMapsReact from "bingmaps-react";
+import BingMapsReact from "../../bing";
 import { useWindowWidth } from '@react-hook/window-size/throttled'
-
+import Spinner from "../components/Spinner";
 
 const MapPage = () => {
   const [exams, setExams] = useState();
-  const [mapWidth, setMapWidth] = useState(500);
+  const [isLoading, setIsLoading] = useState(false);
+  const [mapWidth, setMapWidth] = useState(800);
   const { loggedInUser } = useContext(GlobalContext);
   
   // Recalculates window width each time it changes (throttled at 30fps)
   const windowWidth = useWindowWidth();
-  
-  // Memoising an anonymous function that fixes mapSize at 500px on narrow viewports
-  const adjustMapWidth = useMemo(() => ((windowWidth, mapSize) => 
-    windowWidth < 460 ? 500 
-    : mapWidth)(windowWidth, mapWidth));
-  
-  // Whenever the screen is resized, this call resets map size prior to next paint
-  useLayoutEffect(() => setMapWidth(500), [windowWidth]);
 
 useEffect(() => {
-  getExams(loggedInUser)
+  setIsLoading(true);
+  getExams(loggedInUser, 100)
   .then(res => {
-    setExams(res.exams.map(({latitude, longitude, title, description : subTitle}) => {
+    setIsLoading(false);
+    setExams(res.exams.map(({latitude, longitude, title, description, candidateId, id}) => {
       return {
         center: {
           latitude,
@@ -34,57 +29,57 @@ useEffect(() => {
         },
         options: {
           title,
-          subTitle,
-          enableHoverStyle: true
+          enableHoverStyle: true,
+          color: '#e73c7e',
+          candidateId,
+          description,
+          id
         }
       }
     }));
   })
   .catch(() => {
+    setIsLoading(false);
     /* * */
   });
 }, []);
-// if windowWidth is under 360 => mapSize needs to be fixed at 500
+
 return (
-<main>
-  <h1 className="text-center text-stone-100 font-bold text-4xl mt-20 mx-4">Viewing all exam locations</h1>
-  <p className="text-center text-stone-100 mt-2 mb-16 mx-4">You can zoom and select different view styles.To view information on a specific exam, click on its icon.</p>
+<main className="pb-20">
+  <h1 className="text-center text-stone-100 font-bold text-4xl md:text-5xl mt-20 mx-4">Viewing all exam locations</h1>
+  <p className="text-center text-stone-100 mt-2 mb-16 mx-4 md:text-xl">You can zoom and select different view styles.To view information on a specific exam, click on its icon.</p>
+
+  {isLoading && <Spinner/>}
 
   {/* Buttons */}
+  {!isLoading && windowWidth > 360 &&
   <div className="text-center bg-slate-200 max-w-fit mx-auto mt-10 rounded-lg">
-  <p className="bg-fuchsia-800 text-slate-200 p-2 rounded-t-md">Change size</p>
+  <p className="bg-brightPink p-2 rounded-t-md">Change size</p>
   <div className="flex justify-center">
-  <button onClick={() => setMapWidth(mapWidth + 100)} disabled={windowWidth < 360} className="m-2">
+  <button onClick={() => setMapWidth(mapWidth + 100)} className="m-2">
   <img className="w-7 h-7" src="/images/plus-square-fill.svg"/>
   </button>
-  <button onClick={() => setMapWidth(mapWidth - 100)} disabled={windowWidth < 360} className="m-2">
+  <button onClick={() => setMapWidth(mapWidth - 100)} className="m-2">
   <img className="w-7 h-7" src="/images/dash-square-fill.svg"/>
   </button>
   </div>
-  </div>
+  </div>}
 
   {/* Map */}
-  <div className="flex justify-center m-auto mt-4 w-11/12 max-w-screen-lg">
+  {!isLoading && <div className="flex justify-center m-auto mt-4 w-11/12 max-w-screen-lg">
     <BingMapsReact 
-      bingMapsKey="AgWz7lrowQaBmXiXmXXzepTNUmKwZrEs-gTdNUijBoVZDV0bltjdBdVAvHg8gqod" height={`${adjustMapWidth}px`} width={`${adjustMapWidth}px`}
+      bingMapsKey="AgWz7lrowQaBmXiXmXXzepTNUmKwZrEs-gTdNUijBoVZDV0bltjdBdVAvHg8gqod" height={`${windowWidth < 460 ? 400 : windowWidth < 660 ? 500 : mapWidth}px`} width={`${windowWidth < 460 ? 500 : windowWidth < 660 ? 600 : mapWidth}px`}
       mapOptions={{
         navigationBarMode: "square",
       }}
-      infoboxesWithPushPins = {[
-        {
-          "location":[41.19197, 25.33719], 
-          "addHandler": "click", //on click the pushpin, infobox shown
-          "infoboxOption": { title: '№ на станция', description: '...' },
-          "pushPinOption":{ color: 'yellow' },
-        }
-      ]}
+      onClick={() => console.log('yayaya')}
       pushPinsWithInfoboxes={exams}
       viewOptions={{
         center: {  latitude: 27.98785, longitude: 86.925026 },
-        mapTypeId: "grayscale",
+        mapTypeId: "canvasLight",
         zoom: 1
       }}/>
-  </div>
+  </div>}
 </main>);
 }
 
