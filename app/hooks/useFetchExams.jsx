c66510b9c2c6907888e564c517566dde16767115
3782291211 <Laklouk98@hotmail.com>
2023-04-24 18:01:30 +0100

    Adding custom hook

--- app/hooks/useFetchExams.jsx
+++ app/hooks/useFetchExams.jsx
@@ -2,27 +2,42 @@
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { GlobalContext } from '../context/store';
-import { getExams } from '../api/apiRequests';
+import { getExams, getSingleCandidatesExams } from '../api/apiRequests';
 
-const useFetchExams = () => {
-  const [exams, setExams] = useState();
+const useFetchExams = (limit = 30, id = null) => {
+  const [exams, setExams] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [pageLinks, setPageLinks] = useState({prev: '', next: ''});
   const { loggedInUser } = useContext(GlobalContext);
 
   useEffect(() => {
     setIsLoading(true);
-    getExams(loggedInUser)
-    .then(res => {
+    if (!id) {
+      getExams(loggedInUser, limit)
+      .then(res => {
         setIsLoading(false);
         setExams(res.exams);
-    })
-    .catch(() => {
+        setPageLinks({prev: res.links.prev, next: res.links.next});
+     })
+     .catch(() => {
         setIsLoading(false);
         /* * */
-    });
+     });
+    } else {
+      getSingleCandidatesExams(loggedInUser, id)
+      .then(res => {
+        setIsLoading(false);
+        setExams(res.exams);
+        setPageLinks({prev: res.links.prev, next: res.links.next});
+     })
+     .catch(() => {
+        setIsLoading(false);
+        /* * */
+     });
+    }
   }, []);
 
-  return {exams, isLoading};
+  return {exams, isLoading, pageLinks};
 }
 
 export default useFetchExams;
