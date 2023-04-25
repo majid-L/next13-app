16afec878c7b917f5d974abc2ecbbcf95e5f4255
3782291211 <Laklouk98@hotmail.com>
2023-04-25 17:17:35 +0100

    Removing conditional block

--- app/hooks/useFetchExams.jsx
+++ app/hooks/useFetchExams.jsx
@@ -2,9 +2,9 @@
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { GlobalContext } from '../context/store';
-import { getExams, getSingleCandidatesExams } from '../api/apiRequests';
+import { getSingleCandidatesExams } from '../api/apiRequests';
 
-const useFetchExams = (limit = 30, id = null) => {
+export const useFetchExamsForCandidate = id => {
   const [exams, setExams] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [pageLinks, setPageLinks] = useState({prev: '', next: ''});
@@ -12,18 +12,6 @@ const useFetchExams = (limit = 30, id = null) => {
 
   useEffect(() => {
     setIsLoading(true);
-    if (!id) {
-      getExams(loggedInUser, limit)
-      .then(res => {
-        setIsLoading(false);
-        setExams(res.exams);
-        setPageLinks({prev: res.links.prev, next: res.links.next});
-     })
-     .catch(() => {
-        setIsLoading(false);
-        /* * */
-     });
-    } else {
       getSingleCandidatesExams(loggedInUser, id)
       .then(res => {
         setIsLoading(false);
@@ -34,10 +22,7 @@ const useFetchExams = (limit = 30, id = null) => {
         setIsLoading(false);
         /* * */
      });
-    }
   }, []);
 
   return {exams, isLoading, pageLinks};
 }
-
-export default useFetchExams;
