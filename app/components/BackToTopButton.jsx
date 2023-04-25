77af3042c91224aa91d8a02a20fc9b000db98a8e
3782291211 <Laklouk98@hotmail.com>
2023-04-26 00:19:24 +0100

    Creating a button component

---
+++ app/components/BackToTopButton.jsx
@@ -0,0 +1,10 @@
+const BackToTopButton = () => {
+  return<button onClick={() => window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      })} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-950/80 w-56 px-3 py-1 mt-12 rounded-md">Back to top
+  </button>;
+}
+
+export default BackToTopButton;
