785ed45c12226cdf3ab24a2c96c855f4238f13fb
3782291211 <Laklouk98@hotmail.com>
2023-04-26 11:51:59 +0100

    Adding conditional rendering based on login status

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 import { useState, useContext } from "react";
 import { GlobalContext } from "../context/store";
 import axios from "axios";
+import userIsAdmin from '../helpers/userIsAdmin';
 
 const Header = () => {
 const [showMenu, setShowMenu] = useState(false);
 const [logoutNotification, showLogoutNotification] = useState('');
+const [errorMsg, setErrorMsg] = useState('');
 const { loggedInUser, setLoggedInUser } = useContext(GlobalContext);
 
 const logout = id => axios.get(`https://laravel-php-api.vercel.app/public/api/logout/${id}`, {
@@ -17,10 +19,11 @@ const handleLogout = () => {
   logout(loggedInUser.user.id)
   .then(res => {
     showLogoutNotification(res.msg);
-    setTimeout(() => showLogoutNotification(''), 4000);
+    setTimeout(() => showLogoutNotification(''), 6000);
   })
-  .catch(() => {
-    /* * */
+  .catch(err => {
+    setErrorMsg(err.response?.date?.msg ? err.response.date.msg : err.message);
+    setTimeout(() => setErrorMsg(''), 6000);
   })
   .finally(() => {
     setLoggedInUser({ user: { name: null, id: null, email: null }, token: null });
@@ -43,20 +46,26 @@ return(
                 <img className="w-10 h-10" src="/images/dice-5-fill.svg"/>
                 <p className="text-xl">NXExams</p>
             </div>
+            
             {/* menu items */}
             <div className="hidden md:flex">
               <Link className="font-semibold px-3 pb-2 border-b border-stone-400" href='/'>Home</Link>
-              <Link className="font-semibold px-3 pb-2 border-b border-stone-400" href='/exams'>Exams</Link> 
+              {userIsAdmin(loggedInUser) ? 
+              <>
+              <Link className="font-semibold px-3 pb-2 border-b border-stone-400" href='/exams'>Exams</Link>
               <Link className="font-semibold px-3 pb-2 border-b border-stone-400" href='/candidates'>Candidates</Link>
+              </>
+              : loggedInUser.user?.id ? <Link className="font-semibold px-3 pb-2 border-b border-stone-400" href={`/candidates/${loggedInUser.user?.id}`}>My Exams</Link> : null}
             </div>
+
             {/* login/signup */}
-            {!loggedInUser.user.name && <div className="flex flex-start">
+            {!loggedInUser?.user?.name && <div className="flex flex-start">
             <Link href='/login' className="hidden md:block p-2 px-3 rounded-lg hover:bg-gray-200">Log in</Link>
             <Link href='/signup' className="hidden md:block p-2 rounded-lg hover:bg-gray-200">Signup</Link>
             </div>}
             
             {/*Logged in user*/}
-            {loggedInUser.user.name && <div className="flex flex-start">
+            {loggedInUser?.user?.name && <div className="flex flex-start">
             <Link href='#' className="hidden md:block p-2 mr-4 rounded-full">Hi, {loggedInUser.user.name}.</Link>
             <button onClick={handleLogout} className="hidden md:block p-2 px-3 rounded-lg text-stone-200 bg-gray-800">Logout</button>
             </div>}
@@ -74,20 +83,22 @@ return(
             {!loggedInUser && <><Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/login'>Login</Link>
             <Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/signup'>Signup</Link></>}
             <Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/'>Home</Link>
-            <Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/exams'>Exams</Link>
+            {userIsAdmin(loggedInUser) ? 
+            <><Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/exams'>Exams</Link>
             <Link className="hover:text-orange-500" onClick={() => setShowMenu(false)} href='/candidates'>Candidates</Link>
+            </> 
+            : loggedInUser.user?.id ? <Link className="hover:text-orange-500" href={`/candidates/${loggedInUser.user?.id}`}>My Exams</Link> : null}
         </div>
         </div>
     </nav>
 
-    {logoutNotification && 
-      <div className="w-full bg-green-200 text-center">{logoutNotification}
-    </div>}
+    {logoutNotification ? <div className="w-full bg-green-200 text-center">{logoutNotification}</div> 
+    : errorMsg ?  <div className="w-full bg-red-200 text-center">{errorMsg}</div> : null}
     </div>
-    <div className="flex w-fit ml-6 mt-4 bg-slate-200 p-3 rounded">
+    {userIsAdmin(loggedInUser) && <div className="flex w-fit ml-6 mt-4 bg-slate-200 p-3 rounded">
       <p className="text-sm mr-1">Logged in as <span className="font-bold">admin</span></p>
       <img src='/images/gear.svg'/>
-    </div>
+    </div>}
     </header>);
 };
 
