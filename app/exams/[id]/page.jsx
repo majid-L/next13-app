38fce3bd779b6a30d8976a5f23e9334d9681ce67
3782291211 <Laklouk98@hotmail.com>
2023-04-27 12:06:25 +0100

    Adding handler for PUT requests

--- app/exams/[id]/page.jsx
+++ app/exams/[id]/page.jsx
@@ -1,16 +1,29 @@
 'use client';
-import { getSingleExam } from '../../api/apiRequests';
-import { GlobalContext } from '../../context/store';
+import { getSingleExam, updateExam } from '../../api/apiRequests';
+import { LoggedInUserContext } from '../../context/store';
 import { useState, useEffect, useContext } from 'react';
 import Spinner from '../../components/Spinner';
+import ErrorMessage from '../../components/ErrorMessage';
+import ConfirmationMessage from '../../components/ConfirmationMessage';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import ExamForm from '../../components/ExamForm';
+import { ignoreEmptyFields, formatRequestBody } from '../../helpers/requestBodyManipulators';
+import errorHandler from '../../helpers/errorHandler';
+
 
 const SingleExamPage = ({params}) => {
+// States concerned with fetching the exam
 const [exam, setExam] = useState({date: ''});
+const [refetchData, setRefetchData] = useState(0);
 const [isLoading, setIsLoading] = useState(false);
-const {loggedInUser} = useContext(GlobalContext);
+const {loggedInUser} = useContext(LoggedInUserContext);
 const [errorMsg, setErrorMsg] = useState({value: '', show: true});
+
+// States concerned with PUT operations
+const [examDetails, setExamDetails] = useState(emptyObject);
+const [confirmationMsg, setConfirmationMsg] = useState('');
+const [updateIsLoading, setUpdateIsLoading] = useState(false);
 
 useEffect(() => {
     if (!loggedInUser.user?.id) {
@@ -22,24 +35,40 @@ useEffect(() => {
         setIsLoading(false);
         setExam(exam);
     })
-    .catch(({message, response}) => {
+    .catch(err => {
         setIsLoading(false);
-        if (response?.data?.msg) {
-            setErrorMsg(prev => ({...prev, value: response.data.msg }));
-        } else if (response?.data?.message) {
-            setErrorMsg(prev => ({...prev, value: response.data.message}));
-        } else {
-            setErrorMsg(prev => ({...prev, value: message}));
-        }
+        setErrorMsg(prev => ({...prev, value: errorHandler(err)}));
     })
-}, []);
+}, [refetchData]);
+
+const handleSubmit = e => {
+  e.preventDefault();
+  const validatedInput = ignoreEmptyFields(examDetails);
+  if (validatedInput.length === 0) {
+    setErrorMsg({value : "You cannot submit an empty form.", show: true});
+    return;
+  }
+
+  const body = formatRequestBody(examDetails, exam);
+  updateExam(loggedInUser, params.id, body)
+  .then(() => {
+    setUpdateIsLoading(false);
+    setConfirmationMsg("Exam details have been successfully updated.");
+    setExamDetails(emptyObject);
+    setRefetchData(refetchData + 1);
+  })
+  .catch(err => {
+    setUpdateIsLoading(false);
+    setErrorMsg({value: errorHandler(err), show: true});
+  })
+}
 
 return (
-<main>
+<main className="pb-20">
   <h1 className="text-center text-stone-100 font-bold text-4xl md:text-5xl mt-12">Viewing exam session: {exam.title}</h1>
   <p className="text-center text-stone-100 mt-4 mb-16 mx-4 md:text-xl">To see all exams for {exam.candidateName}, click on the link below.</p>
 
-{isLoading ? <Spinner/> : <div className="bg-white border-1 border-gray-700 shadow-lg shadow-pink-300/40 overflow-hidden rounded-lg w-11/12 max-w-3xl m-auto">
+{isLoading ? <Spinner/> : <div className="bg-white border-1 border-gray-700 shadow-lg shadow-pink-300/40 overflow-hidden rounded-lg w-11/12 mb-16 max-w-3xl m-auto">
 <div className="p-6 pb-4 text-gray-900 text-left">
     <p className="font-bold text-lg">{exam.candidateName} <span className="font-medium">(ID: {exam.candidateId})</span></p>
     <p className="mb-2 pb-2 border-b-2 border-t-indigo-500 text-gray-500">{exam.title} (Exam ID: {exam.id})</p>
@@ -47,13 +76,23 @@ return (
     <Link href={`/candidates/${exam.candidateId}`} className="block w-fit mt-2 text-pink-700 hover:underline hover:text-blue-500">View {exam.candidateName}'s exams</Link>
 </div>
 </div>}
-{/* Error msg */}
-{errorMsg.value && errorMsg.show && <div className="mx-auto my-16 p-4 bg-red-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-red-300/60">
-    <p className="text-lg font-semibold">Somthing went wrong.</p>
-    <p className="italic">{errorMsg.value}</p>
-    <button onClick={() => setErrorMsg(prev => ({...prev, show: false}))} className="bg-red-300 py-2.5 px-5 mt-4 border border-gray-400 rounded-md ">Dismiss</button>
-  </div>}
+{errorMsg.value && errorMsg.show && <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg}/>}
+
+<h2 className="text-center text-stone-100 font-bold text-2xl md:text-3xl mt-12">Update exam details</h2>
+<p className="text-center text-stone-100 mt-4 mb-16 mx-4 md:text-xl">Update as many or as few fields as you'd. Simply click on the "Show" buttons to reveal the input fields.</p>
+<ExamForm formType="PUT" handleSubmit={handleSubmit} examDetails={examDetails} setExamDetails={setExamDetails}/>
+{updateIsLoading && <><p className="text-slate-200 text-center italic">Updating...</p><Spinner margin="mt-4"/></>}
+{confirmationMsg && <ConfirmationMessage confirmationMsg={confirmationMsg} setConfirmationMsg={setConfirmationMsg}/>}
 </main>);
 }
+
+// Used for resetting the form
+const emptyObject = {
+    title: '',
+    description: '',
+    date: '',
+    time: '',
+    location_name: ''
+};
 
 export default SingleExamPage;
