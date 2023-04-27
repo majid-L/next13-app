309cf93fb37a5ebe3289c5bd41fbd624791a833b
3782291211 <Laklouk98@hotmail.com>
2023-04-27 12:04:27 +0100

    Modifying style

--- app/components/BackToTopButton.jsx
+++ app/components/BackToTopButton.jsx
@@ -3,7 +3,7 @@ const BackToTopButton = () => {
         top: 0,
         left: 0,
         behavior: "smooth"
-      })} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-950/80 w-56 px-3 py-1 mt-12 rounded-md">Back to top
+      })} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-950/80 w-56 px-3 py-1 mt-12 rounded-md block mx-auto">Back to top
   </button>;
 }
 
