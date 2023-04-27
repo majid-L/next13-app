69ddb4a8f2902bb5304cb5d24f897c2f4518fd54
3782291211 <Laklouk98@hotmail.com>
2023-04-27 12:05:32 +0100

    Removing useEffect

--- app/context/store.jsx
+++ app/context/store.jsx
@@ -3,8 +3,7 @@
 import { createContext, useState } from "react";
 
 export const LoggedInUserContext = createContext({});
-
-const LoggedInUserProvider = ({children}) => {
+  const LoggedInUserProvider = ({children}) => {
   const [loggedInUser, setLoggedInUser] = useState({ 
         user : 
             { 
@@ -14,19 +13,6 @@ const LoggedInUserProvider = ({children}) => {
             }, 
         token: window.localStorage.getItem('AUTH_TOKEN')
     });
-    
-  //const [loggedInUser, setLoggedInUser] = useState({user: '', token: ''});
-  /*useEffect(() => {
-    setLoggedInUser({ 
-        user : 
-            { 
-                name : window.localStorage.getItem('ACTIVE_USER'),
-                id: window.localStorage.getItem('USER_ID'),
-                email: window.localStorage.getItem('USER_EMAIL')
-            }, 
-        token: window.localStorage.getItem('AUTH_TOKEN')
-    })
-  }, []);*/
     
     return (<LoggedInUserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
         {children}
