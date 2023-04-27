0e1f1667f3121aa062368986560140809ecb0c6e
3782291211 <Laklouk98@hotmail.com>
2023-04-27 01:08:26 +0100

    Removing useEffect from context component

--- app/context/store.jsx
+++ app/context/store.jsx
@@ -2,24 +2,35 @@
 
 import { createContext, useState } from "react";
 
-export const GlobalContext = createContext(null);
+export const LoggedInUserContext = createContext({});
 
-const LoggedInUserContext = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useState(
-        { 
-            user : 
-                { 
-                    name : window.localStorage.getItem('ACTIVE_USER'),
-                    id: window.localStorage.getItem('USER_ID'),
-                    email: window.localStorage.getItem('USER_EMAIL')
-                }, 
-            token: window.localStorage.getItem('AUTH_TOKEN')
-        }
-    );
-
-    return (<GlobalContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+const LoggedInUserProvider = ({children}) => {
+  const [loggedInUser, setLoggedInUser] = useState({ 
+        user : 
+            { 
+                name : window.localStorage.getItem('ACTIVE_USER'),
+                id: window.localStorage.getItem('USER_ID'),
+                email: window.localStorage.getItem('USER_EMAIL')
+            }, 
+        token: window.localStorage.getItem('AUTH_TOKEN')
+    });
+    
+  //const [loggedInUser, setLoggedInUser] = useState({user: '', token: ''});
+  /*useEffect(() => {
+    setLoggedInUser({ 
+        user : 
+            { 
+                name : window.localStorage.getItem('ACTIVE_USER'),
+                id: window.localStorage.getItem('USER_ID'),
+                email: window.localStorage.getItem('USER_EMAIL')
+            }, 
+        token: window.localStorage.getItem('AUTH_TOKEN')
+    })
+  }, []);*/
+    
+    return (<LoggedInUserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
         {children}
-    </GlobalContext.Provider>)
+    </LoggedInUserContext.Provider>)
 }
 
-export default LoggedInUserContext;
+export default LoggedInUserProvider;
