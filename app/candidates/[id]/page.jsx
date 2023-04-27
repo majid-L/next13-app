1c4987902e54a7f8dbc748d44bda5bc7e18e03cb
3782291211 <Laklouk98@hotmail.com>
2023-04-27 12:04:18 +0100

    Adding error and confirmation message components

--- app/candidates/[id]/page.jsx
+++ app/candidates/[id]/page.jsx
@@ -7,26 +7,37 @@ import { getSingleCandidatesExams } from '../../api/apiRequests';
 import { notFound } from 'next/navigation';
 import userIsAdmin from '../../helpers/userIsAdmin';
 import Spinner from '../../components/Spinner';
+import ErrorMessage from '../../components/ErrorMessage';
+import ConfirmationMessage from '../../components/ConfirmationMessage';
 
 const SingleCandidateExams = ({params : { id }}) => {
 
   const [exams, setExams] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState({ value: '', show: true});
+  const [confirmationMsg, setConfirmationMsg] = useState('');
   const { loggedInUser } = useContext(LoggedInUserContext);
+
+  useEffect(() => {
+    if (confirmationMsg) {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  }, []);
 
   useEffect(() => {
     if (!loggedInUser.user?.id || (!userIsAdmin(loggedInUser) && loggedInUser.user?.id !== id)) {
       notFound();
     }
+    setErrorMsg({value: '', show: ''});
     setIsLoading(true);
       getSingleCandidatesExams(loggedInUser, id)
       .then(res => {
         setIsLoading(false);
         setExams(res.exams);
      })
-     .catch(() => {
+     .catch(({response}) => {
         setIsLoading(false);
-        /* * */
+        setErrorMsg(response?.data?.message);
      });
   }, []);
 
@@ -35,7 +46,12 @@ return (
 <main className="text-center pb-20 mx-auto w-11/12 sm:w-5/6">
   <h1 className="text-center text-stone-100 font-bold text-4xl md:text-5xl mt-12 text-shadow-sm">{userIsAdmin(loggedInUser) ? `Viewing exam sessions for ${!isLoading ? exams[0]?.candidateName : '...'}` : 'My exams'}</h1>
   <p className="text-center text-stone-100 mt-4 mb-16 md:text-xl">{userIsAdmin(loggedInUser) ? 'To see all exams for a specific student, click on an exam and you will be taken to the relevant page.' : 'For further details, click on an exam and you will be taken to its information page.'}</p>
-  {exams.length > 0 ? <ExamsList exams={exams} isLoading={isLoading}/> 
+
+  {confirmationMsg && <ConfirmationMessage confirmationMsg={confirmationMsg} setConfirmationMsg={setConfirmationMsg}/>}
+
+  {errorMsg.show && <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg}/>} 
+
+  {exams.length > 0 ? <ExamsList exams={exams} setExams={setExams} loggedInUser={loggedInUser} isLoading={isLoading} setConfirmationMsg={setConfirmationMsg}/> 
   : exams.length === 0 && !isLoading ? <div className="bg-red-400 w-56 rounded p-3 flex mx-auto mt-8">
     <img src="/images/x-circle-fill.svg"/>
     <p className="ml-4">No exams booked.</p>
