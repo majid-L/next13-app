f1d48c316a54cca9b2d1dbb05c7f70806f8b0cda
3782291211 <Laklouk98@hotmail.com>
2023-04-28 09:24:09 +0100

    Modifying styles

--- app/exams/page.jsx
+++ app/exams/page.jsx
@@ -89,8 +89,8 @@ return (<main className="pb-20 mx-auto w-11/12 sm:w-5/6">
 
   {/* list/map view */}
   <div className="flex flex-col w-56 --max-w-720 mx-auto md:w-auto md:flex-row">
-  <button id="prev" onClick={() => setView('list')} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-600/80 grow px-3 py-1 mb-4 rounded-md md:mb-8">View as list</button>
-  <button id="next" onClick={() => setView('map')} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-600/80 w-56 mx-auto grow px-3 py-1 mb-4 rounded-md md:mx-2 md:w-auto md:mb-8">View as map</button>
+  <button onClick={() => setView('list')} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-600/80 grow px-3 py-1 mb-4 rounded-md md:mb-8">View as list</button>
+  <button onClick={() => setView('map')} className="bg-brightPink border-1 border-gray-500 shadow-lg shadow-pink-600/80 w-56 mx-auto grow px-3 py-1 mb-4 rounded-md md:mx-2 md:w-auto md:mb-8">View as map</button>
   {userIsAdmin(loggedInUser) && <Link href='/exams/new' className="bg-orange-300 border-1 border-gray-500 shadow-lg shadow-yellow-500/50 text-center grow px-2 py-1 mb-8 rounded-md">Add new exam</Link>}
   </div>
 
