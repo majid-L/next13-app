afd4cceda983bcd90d8e6a886d5a7915c36e55db
3782291211 <Laklouk98@hotmail.com>
2023-04-28 03:56:05 +0100

    Modifying card layout

--- app/components/ExamsList.jsx
+++ app/components/ExamsList.jsx
@@ -1,11 +1,11 @@
 import Spinner from './Spinner';
 import Link from "next/link";
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { deleteExam } from '../api/apiRequests';
+import { CalendarIcon, MapIcon, TimeIcon } from './Icons';
 
 const ExamsList = ({isLoading, exams, loggedInUser, setConfirmationMsg, setExams}) => {
 const pathname = usePathname();
-const router = useRouter();
 
 const handleDelete = id => {
     deleteExam(loggedInUser, id)
@@ -31,12 +31,13 @@ return isLoading ? <Spinner /> :
         <div className="max-w-md mx-auto sm:px-6 lg:px-8">
              <div className="bg-white border-1 border-gray-700 shadow-lg shadow-pink-300/40 overflow-hidden rounded-lg">
                 <div className="p-6 pb-4 text-gray-900 text-left">
-                    <div onClick={() => router.push(`/exams/${exam.id}`)} className="cursor-pointer">
                     <p className="font-bold text-lg truncate">{exam.candidateName} <span className="font-medium">(ID: {exam.candidateId})</span></p>
                     <p className="mb-2 pb-2 border-b-2 border-t-indigo-500 text-gray-500">{exam.title} (Exam ID: {exam.id})</p>
-                    <p className="pb-2 border-b-2 border-t-indigo-500">{exam.description} is taking place at {exam.locationName} on {exam.date.slice(0, 10)} at {exam.date.slice(10)}.</p>
-                    </div>
-                    {pathname === '/exams' && <Link href={`/candidates/${exam.candidateId}`} className="block mt-2 text-pink-700 hover:underline hover:text-blue-500 overflow-ellipsis truncate">View {exam.candidateName}&apos;s exams</Link>}
+
+                    <p className="pb-2 border-b-2 border-t-indigo-500">{exam.description} is taking place in <span className="flex"><MapIcon/>{exam.locationName}, on</span><span className="flex"><CalendarIcon/>{exam.date.slice(0, 10)}, at</span><span className="flex"><TimeIcon/>{exam.date.slice(10)}.</span></p>
+                    
+                    <Link href={`/exams/${exam.id}`} className="block mt-2 text-blue-600 hover:underline hover:text-red-500">View exam page</Link>
+                    {pathname === '/exams' && <Link href={`/candidates/${exam.candidateId}`} className="block mt-0.5 text-blue-600 hover:underline hover:text-red-500 overflow-ellipsis truncate">View {exam.candidateName}&apos;s exams</Link>}
                      <button onClick={() => handleDelete(exam.id)} className="bg-gray-300 w-full border-2 border-gray-400 mt-2 py-1 px-2 rounded hover:bg-red-400">Cancel session</button>
                 </div>
             </div>
