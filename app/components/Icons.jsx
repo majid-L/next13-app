2353ccabdf0edab9a3fae612e7ccc1cda7760507
3782291211 <Laklouk98@hotmail.com>
2023-04-28 04:51:35 +0100

    Adding user icon

--- app/components/Icons.jsx
+++ app/components/Icons.jsx
@@ -47,3 +47,8 @@ export const MapIcon = () => <svg style={{marginRight: 4, marginTop: 4}} xmlns="
 export const TimeIcon = () => <svg style={{marginRight: 4, marginTop: 4}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
 </svg>;
+
+export const LoggedInUserIcon = () => <svg style={{marginRight: 6, marginTop: 2, color: 'yellow', height: '20px', width: '20px'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-check" viewBox="0 0 16 16">
+<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+<path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>
+</svg>;
