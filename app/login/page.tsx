b5e413a333ef77474b8532d158b8870e263d59c1
3782291211 <Laklouk98@hotmail.com>
2024-01-01 22:05:06 +0000

    Adding tip boxes to auth pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useContext } from "react";
+import { useState, useContext, MouseEventHandler, useEffect } from "react";
 import { LoggedInUserContext } from "../context/store";
 import { login } from '../api/apiRequests';
 import Spinner from "../components/Spinner";
@@ -12,11 +12,24 @@ const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 const [errMsg, setErrMsg] = useState('');
 const [successMsg, setSuccessMsg] = useState(false);
+const [showTip, setShowTip] = useState(false);
 const [loading, setIsLoading] = useState(false);
-const { setLoggedInUser } = useContext(LoggedInUserContext);
+const { loggedInUser, setLoggedInUser } = useContext(LoggedInUserContext);
 
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+useEffect(() => {
+  setShowTip(!loggedInUser.user?.id ? true : false);
+}, []);
+
+const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
+  loginUser(email, password);
+};
+
+const handleSampleUserLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+  loginUser('jr@v3.admin', 'sduhsd8sfsuf');
+}
+
+const loginUser = (email: string, password: string) => {
   setIsLoading(true);
   login(email, password)
   .then((res: Auth) => {
@@ -24,32 +37,20 @@ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     setLoggedInUser(res);
     setIsLoading(false);
     setSuccessMsg(true);
+    setShowTip(false);
     setEmail('');
     setPassword('');
   })
   .catch(err => {
     setIsLoading(false);
     setErrMsg(errorHandler(err));
-  })
-};
+  });
+}
 
 return (
 <main className="pb-20">
   <h1 className="text-center text-stone-100 font-bold text-4xl mt-16">Log in</h1>
   <p className="text-center text-stone-100 mt-4">Log in with your email and password.</p>
-  <form onSubmit={handleSubmit} className="mx-auto my-10 p-4 bg-stone-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-pink-400/60">
-    <div>
-    <label className="block mb-2">Email</label>
-    <input required type="email" onChange={e => setEmail(e.target.value)} value={email} className="h-9 p-2 w-full rounded-md border border-stone-400"/>
-    </div>
-    <div className="mt-4">
-    <label className="block mb-2">Password</label>
-    <input required type="password" onChange={e => setPassword(e.target.value)} value={password}  className="h-9 p-2 w-full rounded-md border border-stone-400"/>
-    </div>
-    <button className="bg-gray-900 text-stone-100 py-2.5 px-5 mt-4 rounded-md active:bg-gray-600">Login</button>
-  </form>
-
-  {loading && <Spinner/>}
 
   {successMsg && <div className="mx-auto my-16 p-4 bg-green-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-green-300/60">
     <p className="text-lg font-semibold">You have logged in successfully.</p>
@@ -62,6 +63,28 @@ return (
     <p>{errMsg}</p>
     <button onClick={() => setErrMsg('')} className="bg-red-300 py-2.5 px-5 mt-4 border border-gray-400 rounded-md ">Dismiss</button>
   </div>}
+
+  {showTip && <div className="mx-auto my-10 p-4 bg-amber-400 w-11/12 max-w-3xl rounded-md shadow-lg shadow-amber-400/60">
+    <div className="flex gap-2 items-center">
+      <img className="h-10" src='/images/info-circle-fill.svg'/>
+      <p>To experience the full features of the app and skip the signup process, log in as a <strong>sample admin user</strong>.</p>
+    </div>
+    <button onClick={handleSampleUserLogin} className="bg-gray-900 text-stone-100 py-2.5 px-5 mt-4 rounded-md active:bg-gray-600">Login as admin</button>
+  </div>}
+
+  <form onSubmit={handleLogin} className="mx-auto my-10 p-4 bg-stone-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-pink-400/60">
+    <div>
+      <label className="block mb-2">Email</label>
+      <input required type="email" onChange={e => setEmail(e.target.value)} value={email} className="h-9 p-2 w-full rounded-md border border-stone-400"/>
+    </div>
+    <div className="mt-4">
+      <label className="block mb-2">Password</label>
+      <input required type="password" onChange={e => setPassword(e.target.value)} value={password}  className="h-9 p-2 w-full rounded-md border border-stone-400"/>
+    </div>
+    <button className="bg-gray-900 text-stone-100 py-2.5 px-5 mt-4 rounded-md active:bg-gray-600">Login</button>
+  </form>
+
+  {loading && <Spinner/>}
 
 </main>);
 };
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -42,14 +42,30 @@ const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 return (
 <main className="pb-20">
   <h1 className="text-stone-100 text-center w-4/5 font-bold text-3xl md:text-4xl mx-auto mt-16">Sign up for a new account.</h1>
-  <p className="text-center text-stone-100 w-4/5 mx-auto mt-4">For demonstration purposes, sign up with an email ending with <span className="font-bold">@v3.admin</span> to enable access to all of the app's features.</p>
+
+  {successMsg && <div className="mx-auto my-16 p-4 bg-green-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-green-300/60">
+    <p className="text-lg font-semibold">Account created successfully.</p>
+    <p>You can now start using your details to sign in to your account.</p>
+    <button onClick={() => setSuccessMsg(false)} className="bg-green-300 py-2.5 px-5 mt-4 border border-gray-400 rounded-md ">Dismiss</button>
+  </div>}
+
+  {errorMsg.show && <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg}/>}
+
+  <div className="mx-auto my-10 p-4 bg-amber-400 w-11/12 max-w-3xl rounded-md shadow-lg shadow-amber-400/60">
+    <div className="flex gap-2 items-center">
+      <img className="h-10" src='/images/info-circle-fill.svg'/>
+      <p>For demonstration purposes, sign up with an email ending with <span className="font-bold">@v3.admin</span> to enable access to all of the app's features.</p>
+    </div>
+    <button onClick={() => setEmail('@v3.admin')} className="bg-gray-900 text-stone-100 py-2.5 px-5 mt-4 rounded-md active:bg-gray-600">Use admin suffix</button>
+  </div>
+
   <form onSubmit={handleSubmit} className="mx-auto my-10 p-4 bg-stone-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-pink-400/60">
     <div>
     <label className="block mb-2">Name</label>
     <input required type="text" onChange={e => setName(e.target.value)} value={name} className="h-9 p-2 w-full rounded-md border border-stone-400"/>
     </div>
     <div className="mt-4">
-    <label className="block mb-2">Email</label>
+    <label className="block mb-2">Email <span className="text-sm">(e.g. your-name<strong>@v3.admin</strong>)</span></label>
     <input required type="email" onChange={e => setEmail(e.target.value)} value={email} className="h-9 p-2 w-full rounded-md border border-stone-400"/>
     </div>
     <div className="mt-4">
@@ -64,14 +80,6 @@ return (
   </form>
 
   {isLoading && <Spinner/>}
-
-  {successMsg && <div className="mx-auto my-16 p-4 bg-green-200 w-11/12 max-w-3xl rounded-md shadow-lg shadow-green-300/60">
-    <p className="text-lg font-semibold">Account created successfully.</p>
-    <p>You can now start using your details to sign in to your account.</p>
-    <button onClick={() => setSuccessMsg(false)} className="bg-green-300 py-2.5 px-5 mt-4 border border-gray-400 rounded-md ">Dismiss</button>
-  </div>}
-
-  {errorMsg.show && <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg}/>}
 </main>);
 };
 
